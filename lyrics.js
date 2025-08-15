7f1227e79d4b1363e14e42b6402d6e91f5ac5cec
Your Name <you@example.com>
2025-08-15 22:01:38 +0800

    first commit

---
+++ lyrics.js
@@ -0,0 +1,192 @@
+const lyrics_timestamps = [
+  {
+    text: "Sungguh tak terasa",
+    start: 17,
+    end: 29
+  },
+  {
+    text: "Sudah tujuh tahun",
+    start: 20,
+    end: 29
+  },
+  {
+    text: "Rotasi waktu hidupku",
+    start: 24,
+    end: 29
+  },
+  {
+    text: "Binar mata yang membiru",
+    start: 30,
+    end: 39,
+  },
+  {
+    text: "Hari hari yang lesu",
+    start: 33,
+    end: 39,
+  },
+  {
+    text: "Aku menunggu kamu",
+    start: 36,
+    end: 39,
+  },
+  {
+    text: "Selalu aku lihat belakang punggungmu",
+    start: 40,
+    end: 51,
+    duration: 4,
+  },
+  {
+    text: "disaat kau lihat belakang punggung pria lain.",
+    start: 45,
+    end: 51,
+    duration: 4,
+  },
+  {
+    text: "Menunggu kau menoleh dan berlari ke arahku dan memelukku",
+    start: 52,
+    end: 65,
+    duration: 6,
+  },
+  {
+    text: "seerat eratnya...",
+    start: 61,
+    end: 65,
+  },
+  {
+    text: "Sudah aku coba",
+    start: 67.5,
+    end: 78,
+  },
+  {
+    text: "untuk menghapusmu!",
+    start: 71,
+    end: 78,
+  },
+  {
+    text: "Naifku hanya jelaga",
+    start: 74,
+    end: 78,
+  },
+  {
+    text: "Dirindu pada siapa??",
+    start: 80,
+    end: 89,
+  },
+  {
+    text: "Ku masih merasakannya!",
+    start: 83,
+    end: 89,
+  },
+  {
+    text: "KAMU MASIH PENYEBABNYA!",
+    start: 86,
+    end: 89,
+  },
+  {
+    text: "Selalu aku lihat belakang punggungmu",
+    start: 90,
+    end: 101,
+    duration: 4,
+  },
+  {
+    text: "disaat kau lihat belakang punggung pria lain.",
+    start: 96,
+    end: 101,
+    duration: 5,
+  },
+  {
+    text: "Menunggu kau menoleh dan berlari ke arahku dan memelukku",
+    start: 103,
+    end: 114,
+    duration: 6,
+  },
+  {
+    text: "seerat eratnya...",
+    start: 111,
+    end: 114,
+  },
+  {
+    text: "Selalu aku lihat belakang punggungmu",
+    start: 115,
+    end: 126,
+    duration: 4,
+  },
+  {
+    text: "disaat kau lihat belakang punggung pria lain.",
+    start: 120.5,
+    end: 126,
+    duration: 5,
+  },
+  {
+    text: "Menunggu kau menoleh dan berlari kearah ku dan memelukku",
+    start: 128,
+    end: 142,
+    duration: 6,
+  },
+  {
+    text: "seerat eratnya...",
+    start: 136,
+    end: 142,
+  },
+  {
+    text: "Mencoba berdamai dengan diriku",
+    start: 165,
+    end: 173,
+  },
+  {
+    text: "tapi kau slalu tau itu",
+    start: 170,
+    end: 173,
+  },
+  {
+    text: "Bagaimana mungkin?",
+    start: 174,
+    end: 176,
+  },
+  {
+    text: "Ketika kau masih jadi satu satunya alasan ku menunggu",
+    start: 177,
+    end: 190,
+    duration: 6,
+  },
+  {
+    text: "di lini waktu ku",
+    start: 186,
+    end: 190,
+  },
+  {
+    text: "Sungguh tak terasa",
+    start: 193,
+    end: 210,
+  },
+  {
+    text: "sudah tujuh tahun",
+    start: 196,
+    end: 210,
+  },
+  {
+    text: "Habiskan masa mudaku",
+    start: 198,
+    end: 210,
+  },
+  {
+    text: "Hanya untuk membuatmu",
+    start: 205,
+    end: 210,
+  },
+  {
+    text: "terkesan kepadaku.",
+    start: 207,
+    end: 210,
+  },
+  {
+    text: "BEGITU BODOHNYA AKU!",
+    start: 212,
+    end: 220,
+  },
+  {
+    text: "Maafin jeruk yang gagal menjagamu...",
+    start: 218,
+    end: 228,
+  },
+]
---
+++ script.js
@@ -0,0 +1,186 @@
+tippy('label:has(#love_button)', {
+  content: 'Kalau suka call me saja',
+})
+tippy('label:has(#loop_button)', {
+  content: 'Bukan tombol repost',
+})
+tippy('label:has(#play_button)', {
+  content: 'Jangan diputar nanti pusing',
+})
+
+
+gsap.registerPlugin(TextPlugin)
+var elem = document.getElementById("root")
+elem.style.display = 'none'
+const assets = document.querySelectorAll('img');
+const progress = document.querySelector('progress')
+progress.max = assets.length;
+let loaded = 0;
+assets.forEach(asset => {
+  asset.onload = () => {
+    loaded++;
+    progress.value = loaded;
+    if (loaded === assets.length) {
+      PowerGlitch.glitch('.glitch')
+      floatAnimation('.floating');
+      elem.style.display = 'flex'
+
+      document.querySelector('.loader').style.display = 'none'
+    }
+  }
+  if (asset.complete) asset.onload();
+
+});
+floatAnimation('.floating-glitch-1');
+floatAnimation('.floating-glitch-2');
+const music = new Audio('song/L - Hal.mp3')
+music.currentTime = lyrics_timestamps[lyrics_timestamps.length - 1].start - 2
+music.currentTime = 16
+music.onloadeddata = function () {
+  music_total_duration.innerText = formatTime(music.duration)
+  song_duration.max = music.duration
+  song_duration.value = music.currentTime;
+}
+let lasttime = 0
+song_duration.oninput = function () {
+  music.currentTime = song_duration.value
+  lasttime = music.currentTime
+  music_current_duration.innerText = formatTime(music.currentTime)
+}
+music.ontimeupdate = function () {
+  if (music.currentTime - lasttime >= 0.5) {
+    song_duration.value = music.currentTime
+    lasttime = music.currentTime
+    music_current_duration.innerText = formatTime(music.currentTime)
+  }
+  lyrics_timestamps.forEach((item, i) => {
+    if (!document.querySelector(`#lyric_${i}`) && music.currentTime > item.start && music.currentTime < item.end + 1) {
+      lyrics_el.innerHTML += `
+        <div class="chat chat-end new-chat" id="lyric_${i}">
+          <div class="chat-bubble text-sm">...</div>
+        </div>
+    `
+
+      gsap.
+        to(`#lyric_${i} .chat-bubble`, {
+          text: {
+            value: item.text,
+          },
+          ease: 'none',
+          duration: item?.duration || 2,
+        })
+      setTimeout(function () {
+        if (document.querySelector(`#lyric_${i}`)) {
+          document.querySelector(`#lyric_${i}`).classList.remove('new-chat')
+        }
+      }, 500)
+    } else if (document.querySelector(`#lyric_${i}`) && !(music.currentTime > item.start && music.currentTime < item.end + 1)) {
+      document.querySelector(`#lyric_${i}`).remove()
+    }
+
+  })
+
+  if (music.currentTime === music.duration) {
+    if (loop_button.checked == true) {
+
+      music.currentTime = 0;
+      lasttime = 0
+      music.play();
+    } else {
+      music.pause();
+      play_button.checked = false;
+
+    }
+  }
+}
+play_button.onchange = function () {
+  if (play_button.checked) {
+    music.play()
+  } else {
+    music.pause()
+  }
+}
+function formatTime(second) {
+  return `${Math.floor(second / 60)}:${(Math.floor(second % 60)).toString().padStart(2, '0')}`
+}
+
+function floatAnimation(el) {
+  const tlCan = gsap.timeline({ repeat: -1, delay: gsap.utils.random(0, 2) });
+
+  tlCan
+    .to(el, 3, { y: '-=30', x: '+=20', rotation: '-=5', ease: 'power1.inOut' })
+    .to(el, 2, { y: '+=30', x: '-=20', rotation: '-=5', ease: 'power1.inOut' })
+    .to(el, 3, { y: '-=20', rotation: '+=5', ease: 'power1.inOut' })
+    .to(el, 3, { y: '+=20', rotation: '+=5', ease: 'power1.inOut' })
+    .to(el, 3, { y: '-=50', ease: 'power1.inOut' })
+    .to(el, 3, { y: '+=50', ease: 'power1.inOut' })
+    .to(el, 3, { y: '-=30', ease: 'power1.inOut' })
+    .to(el, 3, { y: '+=30', ease: 'power1.inOut' })
+    .to(el, 2, { y: '-=30', ease: 'power1.inOut' })
+    .to(el, 2, { y: '+=30', ease: 'power1.inOut' });
+
+  gsap.to(tlCan, { duration: 27, ease: 'power1.inOut' });
+}
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const rains = [{ x: random(0, canvas.width), y: 0, size: random(10, 20) }]
+let lastTime = performance.now()
+let hujanTimer = 0;
+function draw() {
+
+  let currentTime = performance.now()
+  let deltaTime = currentTime - lastTime
+  hujanTimer += deltaTime
+  lastTime = currentTime
+  ctx.strokeStyle = 'rgba(255,255,255,0.25)'
+  ctx.lineWidth = 3
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  rains.forEach((rain, i) => {
+    rain.y += 2
+    ctx.beginPath()
+    ctx.moveTo(rain.x, rain.y)
+    ctx.lineTo(rain.x, rain.y - rain.size)
+    ctx.closePath()
+    ctx.stroke()
+    if (rain.y > canvas.height) {
+      rains.splice(i, 1)
+    }
+  })
+  if (hujanTimer > random(100, 2000)) {
+    hujanTimer = 0
+    rains.push({ x: random(0, canvas.width), y: 0, size: random(10, 20) })
+  }
+  requestAnimationFrame(draw)
+}
+draw()
+function random(min, max) {
+  return Math.round(min + Math.random() * (max - min))
+}
+
+const jeruk_suka = parseInt(localStorage.getItem('jeruk_suka')) == 1
+love_button.checked = jeruk_suka
+love_button.onclick = function (e) {
+  localStorage.setItem('jeruk_suka', (e.target.checked ? 1 : 0))
+}
+window.addEventListener('resize', function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+})
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.detail-image').forEach(el => {
+    el.addEventListener('click', () => {
+      Swal.fire({
+        imageUrl: el.src,
+        showCloseButton: true,
+        showCancelButton: false,
+        showConfirmButton: false,
+        imageAlt: 'Detail Image',
+        imageWidth: 400,
+        didOpen: () => {
+          document.querySelector(".swal2-popup").setAttribute("data-theme", "dark");
+        }
+      })
+    })
+  })
+})
